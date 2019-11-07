8f53972e80cad884e691375a5e3c6e7752151de7
PeshkovaLiz <35866573+PeshkovaLiz@users.noreply.github.com>
2019-11-07 13:58:03 +0300

    Add files via upload

---
+++ CycleSum/CycleSum/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CycleSum
+{
+    class Program
+    {
+        static void Main()
+        {
+            /* в данной программе не учитывается вариант, где необходимо 3 итерации сложения,
+             т.к. эти числа выходят за пределы допустимых чисел int */
+
+            int n, m;  //n - циклическая сумма, m - верхняя граница чисел
+            int s=0;  //сумма одного конкретного числа (исп. в цикле)
+            int k = 0;   //переменная-условие счетчика
+            int temp = 0; 
+
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Введите N (значение циклической суммы)");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите M (верхняя граница чисел)");
+            m = Convert.ToInt32(Console.ReadLine());
+
+            numbers.Add(n);   //добавление первого числа, для которого не требуется сложение цифр
+
+            for (int i=10; i<=m; i++)
+            {
+                s = 0;
+                k = i;
+
+                while(k >= 1)
+                {
+                    s = s + k % 10;
+                    k = k / 10;
+                }
+                if (s > 9)  //если сумма двузначная, и нужна еще одна итерация
+                {
+                    temp = s;
+                    s = 0;
+                    while (temp >= 1)
+                    {
+                        s = s + temp % 10;
+                        temp = temp / 10;
+                    }
+                }
+                if (s==n)
+                {
+                    numbers.Add(i);  
+                }
+
+            }
+            foreach (int num in numbers)
+                Console.WriteLine(num);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestSerialize/TestSerialize/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSerialize
+{
+    class ListNode
+    {
+        public ListNode Prev;
+        public ListNode Next;
+        public ListNode Rand; // произвольный элемент внутри списка
+        public string Data;
+    }
+
+    class ListRand
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        public void Serialize(FileStream s)
+        {
+            List<ListNode> arr = new List<ListNode>();
+            ListNode temp = new ListNode();
+            temp = Head;
+
+            //запись списка в List<>
+            do
+            {
+                arr.Add(temp);
+                temp = temp.Next;
+            } while (temp != null);
+
+            //запись в файл
+            //запись ведется с разделителем ":", слева данные, справа индексы случайных элементов
+            using (StreamWriter w = new StreamWriter(s))
+                foreach (ListNode n in arr)
+                    w.WriteLine(n.Data.ToString() + ":" + arr.IndexOf(n.Rand).ToString());
+        }
+
+        public void Deserialize(FileStream s)
+        {
+            List<ListNode> arr = new List<ListNode>();
+            ListNode temp = new ListNode();
+            Count = 0;
+            Head = temp;
+            string line;
+
+            //чтение файла и запись элементов в List<>
+            try
+            {
+                using (StreamReader sr = new StreamReader(s))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.Equals("")==false)
+                        {
+                            Count++;
+                            temp.Data = line;
+                            ListNode next = new ListNode();
+                            temp.Next = next;
+                            arr.Add(temp);
+                            next.Prev = temp;
+                            temp = next;
+                        }
+                    }
+                }
+
+                //"хвост" списка
+                Tail = temp.Prev;
+                Tail.Next = null;
+
+                //запись восстановленных элементов в список с ссылками на случайный элемент
+                foreach (ListNode n in arr)
+                {
+                    n.Rand = arr[Convert.ToInt32(n.Data.Split(':')[1])];
+                    n.Data = n.Data.Split(':')[0];
+                }
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Press Enter to exit.");
+                Console.Read();
+                Environment.Exit(0);
+            }
+        }
+    }
+
+    class Program
+    {
+        static Random rand = new Random();
+
+        //для создания следующего элемента списка
+        static ListNode AddNode(ListNode prev)
+        {
+            ListNode result = new ListNode
+            {
+                Prev = prev,
+                Next = null,
+                Data = rand.Next(0, 100).ToString()
+            };
+            prev.Next = result;
+            return result;
+        }
+        //для генерации ссылок на случайные элементы
+        static ListNode RandomNode(ListNode _head, int _length)
+        {
+            int k = rand.Next(0, _length);
+            int i = 0;
+            ListNode result = _head;
+            while (i < k)
+            {
+                result = result.Next;
+                i++;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            //кол-во элементов в списке
+            int length = 20;
+
+            //первый элемент
+            ListNode head = new ListNode();
+            ListNode tail = new ListNode();
+            ListNode temp = new ListNode();
+            temp = head;
+            head.Data = rand.Next(0, 1000).ToString(); //записывается случайное число в "голову" списка
+
+            tail = head;
+
+            for (int i = 1; i < length; i++)
+            { tail = AddNode(tail); }
+ 
+            //создание ссылки на случайный элемент
+            for (int i = 0; i < length; i++)
+            {
+                temp.Rand = RandomNode(head, length);
+                temp = temp.Next;
+            }
+
+            ListRand serializeList = new ListRand
+            {
+                Head = head,
+                Tail = tail,
+                Count = length
+            };
+
+            // получаем поток, куда будем записывать сериализованные данные
+            FileStream fs = new FileStream("file.dat", FileMode.OpenOrCreate);
+            serializeList.Serialize(fs);   //сериализация
+
+            //десериализация в List<>
+            ListRand deserializeList = new ListRand();
+            try
+            {
+                fs = new FileStream("file.dat", FileMode.Open);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Press Enter to exit.");
+                Console.Read();
+                Environment.Exit(0);
+            }
+            deserializeList.Deserialize(fs);
+
+            //проверка, корректности сериализации и десериализации
+            //для этого сравниваем "хвосты" списков до сериализации и после десериализации
+            if (deserializeList.Tail.Data == serializeList.Tail.Data) Console.WriteLine("Данные сериализованы и десериализованы успешно");
+            Console.Read();
+        }
+    }
+}
